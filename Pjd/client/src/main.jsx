cc99b075ed8a3204b69c94ede705fb141db7db46
KAOUTAR EL MFAKER <125546934+Kelmfaker@users.noreply.github.com>
2025-11-19 04:56:41 +0100

    Delete Pjd directory

--- Pjd/client/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-const container = document.getElementById('root') || document.body.appendChild(document.createElement('div'));
-container.id = 'root';
-createRoot(container).render(<App />)
